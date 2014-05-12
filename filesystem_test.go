006a0cc07ddba9142c61f8a63d2410cdd17db2c0
A. Diamond <diamond@sheprador.com>
2014-05-12 15:40:51 -0400

    Test filesystem with a more specific path

--- filesystem_test.go
+++ filesystem_test.go
@@ -2,12 +2,17 @@ package bagman_test
 
 import (
 	"testing"
+	"runtime"
 	"github.com/APTrust/bagman"
 )
 
 
+// Make sure we can find the number of available bytes
+// on the current device. Var filename contains the full
+// path to this source file.
 func TestAvailableSpace(t *testing.T) {
-	_, err := bagman.AvailableSpace("/")
+	_, filename, _, _ := runtime.Caller(0)
+	_, err := bagman.AvailableSpace(filename)
 	if err != nil {
 		t.Error("Cannot get file system's available space: %v\n", err)
 	}
