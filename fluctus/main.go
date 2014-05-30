78a42abe9e0aea79218acc642e3450fed0169ff7
A. Diamond <diamond@sheprador.com>
2014-05-30 16:12:29 -0400

    Updated test harness. Needs to move to client_test.

--- fluctus/main.go
+++ fluctus/main.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"time"
 	"github.com/APTrust/bagman/fluctus/client"
+	"github.com/APTrust/bagman"
 )
 
+// TODO: Move this into client_test
 func main() {
 	logger := log.New(os.Stdout, "", 0)
 	client, err := client.New("http://localhost:3000",
@@ -19,15 +21,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	bag_date, _ := time.Parse("2006-01-02T15:04:05.000Z", "2014-05-01T12:00:00.000Z")
-	status, err := client.GetBagStatus("SAMPLE_ETAG", "SAMPLE_NAME", bag_date)
+	bagDate, _ := time.Parse("2006-01-02T15:04:05.000Z", "2014-05-01T12:00:00.000Z")
+	status := &bagman.ProcessStatus{
+		0,
+		"sample_uva_bag.tar",
+		"aptrust.receiving.virginia.edu",
+		"9876543210",
+		bagDate,
+		"University of Virginia",
+		time.Now().UTC(),
+		"Note for test entry",
+		"Ingest",
+		"Record",
+		"Succeeded",
+		"Succeeded"}
+
+
+	remoteStatus, err := client.GetBagStatus(status.ETag, status.Name, status.BagDate)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(status)
+	fmt.Println("Remote status is", remoteStatus)
+	if remoteStatus != nil {
+		status = remoteStatus
+	}
 	status.Date = time.Now().UTC()
 	status.Action = "Ingest"
+	status.Status = "Succeeded"
+	status.Outcome = "Succeeded"
 	err = client.UpdateBagStatus(status)
 	if err != nil {
 		fmt.Println(err)
