68dc3411e496c2cac8d54e76a8d32f1ea2ba80a4
A. Diamond <diamond@sheprador.com>
2014-06-09 12:35:34 -0400

    Added test for LoadRelativeFile

--- util_test.go
+++ util_test.go
@@ -3,16 +3,18 @@ package bagman_test
 import (
     "testing"
     "os"
+    "path/filepath"
     "github.com/APTrust/bagman"
 )
 
 func TestBagmanHome(t *testing.T) {
     bagmanHome := os.Getenv("BAGMAN_HOME")
     goHome := os.Getenv("GO_HOME")
+    defer os.Setenv("BAGMAN_HOME", bagmanHome)
+    defer os.Setenv("GO_HOME", goHome)
 
     // Should use BAGMAN_HOME, if it's set...
     os.Setenv("BAGMAN_HOME", "/bagman_home")
-    defer os.Setenv("BAGMAN_HOME", bagmanHome)
     bagmanHome, err := bagman.BagmanHome()
     if err != nil {
         t.Error(err)
@@ -26,7 +28,6 @@ func TestBagmanHome(t *testing.T) {
 
     // Otherwise, should use GO_HOME
     os.Setenv("GO_HOME", "/go_home")
-    defer os.Setenv("GO_HOME", goHome)
     bagmanHome, err = bagman.BagmanHome()
     if err != nil {
         t.Error(err)
@@ -38,9 +39,20 @@ func TestBagmanHome(t *testing.T) {
     }
     os.Setenv("GO_HOME", "")
 
+    // Without BAGMAN_HOME and GO_HOME, we should get an error
     bagmanHome, err = bagman.BagmanHome()
     if err == nil {
         t.Error("BagmanHome should have an thrown exception.")
     }
+}
 
+func TestLoadRelativeFile(t *testing.T) {
+    path := filepath.Join("testdata", "result_good.json")
+    data, err := bagman.LoadRelativeFile(path)
+    if err != nil {
+        t.Error(err)
+    }
+    if data == nil || len(data) == 0 {
+        t.Errorf("Read no data out of file '%s'", path)
+    }
 }
