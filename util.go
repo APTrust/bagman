d691a690345f0735339f5c1d280ea1811766cafb
A. Diamond <diamond@sheprador.com>
2014-07-29 14:53:09 -0400

    Added SynchronizedMap

--- util.go
+++ util.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "io/ioutil"
     "os"
+    "sync"
     "path/filepath"
 	"encoding/json"
 )
@@ -55,4 +56,66 @@ func LoadResult(filename string) (result *ProcessResult, err error) {
         return nil, err
     }
     return result, nil
+}
+
+type SynchronizedMap struct {
+	data        map[string]string
+	mutex       *sync.RWMutex
+}
+
+func NewSynchronizedMap()(*SynchronizedMap) {
+	return &SynchronizedMap {
+		data:  make(map[string]string),
+		mutex: &sync.RWMutex{},
+	}
+}
+
+func (syncMap *SynchronizedMap) HasKey (key string) (bool) {
+	syncMap.mutex.RLock()
+	_, hasKey := syncMap.data[key]
+	syncMap.mutex.RUnlock()
+	return hasKey
+}
+
+func (syncMap *SynchronizedMap) Add (key, value string) {
+	syncMap.mutex.Lock()
+	syncMap.data[key] = value
+	syncMap.mutex.Unlock()
+}
+
+func (syncMap *SynchronizedMap) Get (key string) (string) {
+	syncMap.mutex.RLock()
+	value, _ := syncMap.data[key]
+	syncMap.mutex.RUnlock()
+	return value
+}
+
+func (syncMap *SynchronizedMap) Delete (key string) {
+	syncMap.mutex.Lock()
+	delete(syncMap.data, key)
+	syncMap.mutex.Unlock()
+}
+
+func (syncMap *SynchronizedMap) Keys () ([]string) {
+	keys := make([]string, len(syncMap.data))
+	syncMap.mutex.RLock()
+	i := 0
+	for key, _ := range syncMap.data {
+		keys[i] = key
+		i += 1
+	}
+	syncMap.mutex.RUnlock()
+	return keys
+}
+
+func (syncMap *SynchronizedMap) Values () ([]string) {
+	vals := make([]string, len(syncMap.data))
+	syncMap.mutex.RLock()
+	i := 0
+	for _, val := range syncMap.data {
+		vals[i] = val
+		i += 1
+	}
+	syncMap.mutex.RUnlock()
+	return vals
 }
--- util_test.go
+++ util_test.go
@@ -56,3 +56,56 @@ func TestLoadRelativeFile(t *testing.T) {
         t.Errorf("Read no data out of file '%s'", path)
     }
 }
+
+func TestSyncMap(t *testing.T) {
+	syncMap := bagman.NewSynchronizedMap()
+	keys1 := [...]string { "1", "2", "3", "4", "5" }
+	keys2 := [...]string { "6", "7", "8", "9", "10" }
+	go testSyncMap(t, syncMap, keys1)
+	go testSyncMap(t, syncMap, keys2)
+}
+
+func testSyncMap(t *testing.T, syncMap *bagman.SynchronizedMap, keys [5]string) {
+	for i, key := range keys {
+		syncMap.Add(key, key)
+		if syncMap.HasKey(key) == false {
+			t.Errorf("SyncMap should have key %s", key)
+		}
+		if syncMap.Get(key) != key {
+			t.Errorf("SyncMap key %s has value %s, expected %s", key, syncMap.Get(key), key)
+		}
+		if len(syncMap.Keys()) < i {
+			t.Errorf("SyncMap should have at least %d keys, but it has %d", i, len(syncMap.Keys()))
+		}
+		if len(syncMap.Values()) < i {
+			t.Errorf("SyncMap should have at least %d values, but it has %d", i, len(syncMap.Values()))
+		}
+	}
+}
+
+func TestSyncMapDelete(t *testing.T) {
+	syncMap := bagman.NewSynchronizedMap()
+	keys := [...]string { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+
+	for _, key := range keys {
+		syncMap.Add(key, key)
+	}
+	if len(syncMap.Keys()) != len(keys) {
+		t.Errorf("SyncMap should have %d keys, but it has %d", len(keys), len(syncMap.Keys()))
+	}
+	if len(syncMap.Values()) != len(keys) {
+		t.Errorf("SyncMap should have %d values, but it has %d", len(keys), len(syncMap.Values()))
+	}
+	for _, key := range keys {
+		syncMap.Delete(key)
+		if syncMap.HasKey(key) == true {
+			t.Errorf("SyncMap should not have key %s", key)
+		}
+	}
+	if len(syncMap.Keys()) != 0 {
+		t.Errorf("SyncMap should have 0 keys, but it has %d", len(syncMap.Keys()))
+	}
+	if len(syncMap.Values()) != 0 {
+		t.Errorf("SyncMap should have 0 values, but it has %d", len(syncMap.Values()))
+	}
+}
