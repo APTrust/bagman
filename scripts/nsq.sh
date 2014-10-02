bf23d76e1fb4349db22434a11358fc41fdf9f580
A. Diamond <diamond@sheprador.com>
2014-10-02 15:31:01 -0400

    Removed apt scripts for supervisord

--- scripts/nsq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Run NSQ services
-# Load bash_profile first to set correct PATH
-#
-source /home/ubuntu/.bash_profile
-cd /home/ubuntu/go/src/github.com/APTrust/bagman/nsq/
-go run service.go -config /home/ubuntu/go/src/github.com/APTrust/bagman/nsq/nsqd.demo.config
