4e577ad5579ef73adf3f07a671cc592aa4510430
A. Diamond <diamond@sheprador.com>
2014-10-09 14:09:20 -0400

    Added metadatarecord_test.go

---
+++ bagman/metadatarecord_test.go
@@ -0,0 +1,22 @@
+package bagman_test
+
+import (
+	"github.com/APTrust/bagman"
+	"testing"
+)
+
+func TestMetadataRecordSucceeded(t *testing.T) {
+	record := &bagman.MetadataRecord{
+		Type:         "PremisEvent",
+		Action:       "fixity_generation",
+		EventObject:  "data/ORIGINAL/1",
+		ErrorMessage: "",
+	}
+	if record.Succeeded() == false {
+		t.Error("MetadataRecord.Succeeded() returned false when it should return true")
+	}
+	record.ErrorMessage = "Server returned status code 403: forbidden"
+	if record.Succeeded() == true {
+		t.Error("MetadataRecord.Succeeded() returned true when it should return false")
+	}
+}
