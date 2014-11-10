6581723fcf883962b0446ead445832e19fc715fa
A. Diamond <diamond@sheprador.com>
2014-11-10 16:30:22 -0500

    Enqueue can now queue any type of object

--- bagman/nsqclient.go
+++ bagman/nsqclient.go
@@ -9,17 +9,16 @@ import (
 )
 
 // Sends the JSON of a result object to the specified queue.
-func Enqueue(nsqdHttpAddress, topic string, result *ProcessResult) error {
-	key := result.S3File.Key.Key
+func Enqueue(nsqdHttpAddress, topic string, object interface{}) error {
 	url := fmt.Sprintf("%s/put?topic=%s", nsqdHttpAddress, topic)
-	json, err := json.Marshal(result)
+	json, err := json.Marshal(object)
 	if err != nil {
-		return fmt.Errorf("Error marshalling result for '%s' to JSON for file: %v", key, err)
+		return fmt.Errorf("Error marshalling data to JSON for file: %v", err)
 	}
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(json))
 
 	if err != nil {
-		return fmt.Errorf("Nsqd returned an error when queuing '%s': %v", key, err)
+		return fmt.Errorf("Nsqd returned an error when queuing data: %v", err)
 	}
 	if resp == nil {
 		return fmt.Errorf("No response from nsqd at '%s'. Is it running?", url)
@@ -35,9 +34,8 @@ func Enqueue(nsqdHttpAddress, topic string, result *ProcessResult) error {
 		if len(body) > 0 {
 			bodyText = string(body)
 		}
-		return fmt.Errorf("nsqd returned status code %d when attempting to queue %s. " +
-			"Response body: %s",
-			resp.StatusCode, key, bodyText)
+		return fmt.Errorf("nsqd returned status code %d when attempting to queue data. " +
+			"Response body: %s", resp.StatusCode, bodyText)
 	}
 	return nil
 }
