c2f03943fdee469beca43454b47b1c4cf78bd8fd
A. Diamond <diamond@sheprador.com>
2014-11-10 16:51:02 -0500

    Start failed_fixity worker along with fixity checker

--- scripts/fixity_check.sh
+++ scripts/fixity_check.sh
@@ -19,8 +19,16 @@ cd ~/go/src/github.com/APTrust/bagman/apps/apt_fixity
 go run apt_fixity.go -config apd4n &
 FIXITY_PID=$!
 
+echo "Starting failed fixity worker"
+cd ~/go/src/github.com/APTrust/bagman/apps/apt_failed_fixity
+go run apt_failed_fixity.go -config apd4n &
+FAILED_FIXITY_PID=$!
+
 kill_all()
 {
+    echo "Shutting down failed fixity worker"
+    kill -s SIGINT $FAILED_FIXITY_PID
+
     echo "Shutting down fixity checker"
     kill -s SIGINT $FIXITY_PID
 
