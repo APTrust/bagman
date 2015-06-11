5a8205a7ee1e39e3737dbe31577fa5940b2876b9
A. Diamond <diamond@sheprador.com>
2015-06-11 16:37:13 -0400

    Added dpn_copy.go

--- apps/dpn_copy/dpn_copy.go
+++ apps/dpn_copy/dpn_copy.go
@@ -4,22 +4,26 @@ import (
 	"github.com/APTrust/bagman/workers"
 )
 
-// dpn_package builds a DPN bag from an APTrust object.
+// dpn_copy copies bags from remote nodes so we can replicate
+// those bags. The copy is done via rsync over ssh.
 func main() {
 	procUtil := workers.CreateProcUtil()
 	procUtil.MessageLog.Info("Connecting to NSQLookupd at %s", procUtil.Config.NsqLookupd)
 	procUtil.MessageLog.Info("NSQDHttpAddress is %s", procUtil.Config.NsqdHttpAddress)
-	consumer, err := workers.CreateNsqConsumer(&procUtil.Config, &procUtil.Config.DPNPackageWorker)
+	consumer, err := workers.CreateNsqConsumer(&procUtil.Config, &procUtil.Config.DPNCopyWorker)
 	if err != nil {
 		procUtil.MessageLog.Fatal(err.Error())
 	}
-	procUtil.MessageLog.Info("dpn_package started")
+	procUtil.MessageLog.Info("dpn_copy started")
 	dpnConfig, err := dpn.LoadConfig("dpn/dpn_config.json")
 	if err != nil {
 		procUtil.MessageLog.Fatal(err.Error())
 	}
-	packager := dpn.NewPackager(procUtil, dpnConfig)
-	consumer.AddHandler(packager)
+	copier, err := dpn.NewCopier(procUtil, dpnConfig)
+	if err != nil {
+		procUtil.MessageLog.Fatal(err.Error())
+	}
+	consumer.AddHandler(copier)
 	consumer.ConnectToNSQLookupd(procUtil.Config.NsqLookupd)
 
 	// This reader blocks until we get an interrupt, so our program does not exit.
--- scripts/build.sh
+++ scripts/build.sh
@@ -92,6 +92,10 @@ echo "building apt_failed_replication"
 cd "${BAGMAN_HOME}/apps/apt_failed_replication"
 go build -o ${BAGMAN_BIN}/apt_failed_replication apt_failed_replication.go
 
+echo "building dpn_copy"
+cd "${BAGMAN_HOME}/apps/dpn_copy"
+go build -o ${BAGMAN_BIN}/dpn_copy dpn_copy.go
+
 echo "building dpn_package"
 cd "${BAGMAN_HOME}/apps/dpn_package"
 go build -o ${BAGMAN_BIN}/dpn_package dpn_package.go
